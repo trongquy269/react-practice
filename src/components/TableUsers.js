5f54f9a36e6411e09587088a42c577322d02e6ee
Ng Trong Quy <quyb2017000@student.ctu.edu.vn>
2023-05-25 00:06:46 +0700

    customize axios, create table show data

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,5 +1,51 @@
+import { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
+import { fetchAllUser } from '../services/UserService';
+
 const TableUsers = (props) => {
-	return <>TableUsers...</>;
+	const [listUsers, setListUsers] = useState([]);
+
+	useEffect(() => {
+		// Call api
+		getUsers();
+	}, []);
+
+	const getUsers = async () => {
+		const res = await fetchAllUser();
+
+		if (res && res.data) {
+			setListUsers(res.data);
+		}
+	};
+
+	return (
+		<Table
+			striped
+			bordered
+			hover
+		>
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>Email</th>
+				</tr>
+			</thead>
+			<tbody>
+				{listUsers &&
+					listUsers.length > 0 &&
+					listUsers.map((user, index) => (
+						<tr key={`users-${index}`}>
+							<td>{user.id}</td>
+							<td>{user.first_name}</td>
+							<td>{user.last_name}</td>
+							<td>{user.email}</td>
+						</tr>
+					))}
+			</tbody>
+		</Table>
+	);
 };
 
 export default TableUsers;
