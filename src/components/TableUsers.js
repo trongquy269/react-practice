7c7aed5ab11ef67bc5f5c93d55198a16bddb5f6e
Ng Trong Quy <quyb2017000@student.ctu.edu.vn>
2023-05-25 00:38:31 +0700

    react paginate

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,50 +1,82 @@
 import { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import { fetchAllUser } from '../services/UserService';
+import ReactPaginate from 'react-paginate';
 
 const TableUsers = (props) => {
 	const [listUsers, setListUsers] = useState([]);
+	const [totalUsers, setTotalUsers] = useState(0);
+	const [totalPages, setTotalPages] = useState(0);
 
 	useEffect(() => {
 		// Call api
-		getUsers();
+		getUsers(1);
 	}, []);
 
-	const getUsers = async () => {
-		const res = await fetchAllUser();
+	const getUsers = async (page) => {
+		const res = await fetchAllUser(page);
 
 		if (res && res.data) {
+			setTotalUsers(res.total);
+			setTotalPages(res.total_pages);
 			setListUsers(res.data);
 		}
 	};
 
+	const handlePageClick = (event) => {
+		getUsers(+event.selected + 1);
+	};
+
 	return (
-		<Table
-			striped
-			bordered
-			hover
-		>
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>First Name</th>
-					<th>Last Name</th>
-					<th>Email</th>
-				</tr>
-			</thead>
-			<tbody>
-				{listUsers &&
-					listUsers.length > 0 &&
-					listUsers.map((user, index) => (
-						<tr key={`users-${index}`}>
-							<td>{user.id}</td>
-							<td>{user.first_name}</td>
-							<td>{user.last_name}</td>
-							<td>{user.email}</td>
-						</tr>
-					))}
-			</tbody>
-		</Table>
+		<div>
+			<Table
+				striped
+				bordered
+				hover
+			>
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					{listUsers &&
+						listUsers.length > 0 &&
+						listUsers.map((user, index) => (
+							<tr key={`users-${index}`}>
+								<td>{user.id}</td>
+								<td>{user.first_name}</td>
+								<td>{user.last_name}</td>
+								<td>{user.email}</td>
+							</tr>
+						))}
+				</tbody>
+			</Table>
+
+			<ReactPaginate
+				nextLabel='next >'
+				onPageChange={handlePageClick}
+				pageRangeDisplayed={3}
+				marginPagesDisplayed={2}
+				pageCount={totalPages}
+				previousLabel='< previous'
+				pageClassName='page-item'
+				pageLinkClassName='page-link'
+				previousClassName='page-item'
+				previousLinkClassName='page-link'
+				nextClassName='page-item'
+				nextLinkClassName='page-link'
+				breakLabel='...'
+				breakClassName='page-item'
+				breakLinkClassName='page-link'
+				containerClassName='pagination'
+				activeClassName='active'
+				renderOnZeroPageCount={null}
+			/>
+		</div>
 	);
 };
 
